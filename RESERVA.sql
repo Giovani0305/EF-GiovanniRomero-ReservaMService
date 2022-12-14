8546dd3a0d25e0e78d48cf03028e0b6c941e178f
Giovani0305 <116698863+Giovani0305@users.noreply.github.com>
2022-12-14 08:11:03 -0500

    Add files via upload

---
+++ RESERVA.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbhoteleria.reserva VALUES (1,7200);
+INSERT INTO dbhoteleria.reserva VALUES (2,600);
+INSERT INTO dbhoteleria.reserva VALUES (3,1400);
+INSERT INTO dbhoteleria.reserva VALUES (4,3100);
+INSERT INTO dbhoteleria.reserva VALUES (5,400);
